0bb30ae835840ca3e97e3f466e542416f4c114d4
Alisafa60 <alisafa6100@gmail.com>
2023-11-26 18:28:14 +0200

    simple search using exact tokens match regardless of letter case

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const natural = require('natural');
 
 class Document{
     constructor(title, content){
@@ -17,10 +18,13 @@ class DocumentCollection{
     }
 
     addDocumentStoredLocally(filePaths){
+        const tokenizer = new natural.WordTokenizer();
+
         filePaths.forEach(filePath => {
-            const documentContent = fs.readFileSync(filePath, 'utf-8');
+            const documentContent = fs.readFileSync(filePath, 'utf-8'); //read the content
+            const tokens = tokenizer.tokenize(documentContent); //tokenize the content
             const fileName = filePath.split('/').pop();
-            const document = new Document(fileName, documentContent);
+            const document = new Document(fileName, tokens);
             this.addDocument(document);
         })
     }
@@ -35,14 +39,33 @@ class searchEngine{
         this.documentCollection = documentCollection;
     }
     
+    search(querry){
+        const tokenizer = new natural.WordTokenizer();
+        const querryTokens = tokenizer.tokenize(querry.toLowerCase());
+
+        this.documentCollection.documents.forEach(document => {
+            const documentTokens = document.content.map(token => token.toLowerCase());
+            const querryTokenFound = querryTokens.some(querryToken => documentTokens.includes(querryToken))
+            if(querryTokenFound)
+                console.log(`Document: ${document.title}, querry token found: ${querry}`);
+        })
+    }
 }
 
-const filePaths = ['C:/Users/Hamze/Desktop/test/test-document.txt'];
+const filePaths = ['C:/Users/Hamze/Desktop/test/fastFood.txt', 'C:/Users/Hamze/Desktop/test/bikes.txt'];
 
 const documentCollection = new DocumentCollection();
 
 documentCollection.addDocumentStoredLocally(filePaths);
 
-const document1 = documentCollection.getDocumentByTitle('test-document.txt');
+const document1 = documentCollection.getDocumentByTitle('fastFood.txt');
+const document2 = documentCollection.getDocumentByTitle('bikes.txt')
 
-console.log('document read:', document1);
+const searchEngin = new searchEngine(documentCollection);
+
+searchEngin.search("burgers");
+
+
+
+// console.log('document 1: ', document1);
+// console.log('document 2: ', document2);
