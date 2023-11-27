1f66aab01634c2bbe1b4e2f951324f00f616a3b9
Alisafa60 <alisafa6100@gmail.com>
2023-11-27 15:48:03 +0200

    added synonyms functionality

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ class DocumentCollection{
 class SearchEngine{
     constructor(documentCollection){
         this.documentCollection = documentCollection;
-        this.stemmer = natural.PorterStemmer
+        this.stemmer = natural.PorterStemmer //stemming for finding root word
     }
 
-    async querrySynonyms(querryTokens) {
+    async querrySynonyms(querryTokens) { 
         const synonyms = await Promise.all(querryTokens.map(token =>
             axios.get(`https://api.datamuse.com/words?rel_syn=${encodeURIComponent(token)}`)
                 .then(response => response.data.map(item => item.word))
@@ -52,7 +52,7 @@ class SearchEngine{
         return querryTokens.flatMap((token, index) => [token, ...synonyms[index]]);
     }
     
-    calculateLevenshteinDistance(str1, str2){
+    calculateLevenshteinDistance(str1, str2){ 
         return natural.LevenshteinDistance(str1, str2);
     }
     
@@ -63,32 +63,33 @@ class SearchEngine{
     search(querry){
         const tokenizer = new natural.WordTokenizer();
         const querryTokens = tokenizer.tokenize(querry.toLowerCase());
-        
-
-        this.documentCollection.documents.forEach(document => {
-            const distances = [];
-            const wordMatches = [];
-            const documentTokens = this.stemTokens(document.content); 
-            const stemmedQuerryTokens = this.stemTokens(querryTokens);
-            
-           
-            stemmedQuerryTokens.forEach(querryToken => {
-            const tokenDistances = documentTokens.map(documentToken => 
-                    this.calculateLevenshteinDistance(querryToken, documentToken)
-                );
-                const minDistance = Math.min(...tokenDistances);
-                distances.push(minDistance)
-                const wordMatch = documentTokens[tokenDistances.indexOf(minDistance)];
-                wordMatches.push(wordMatch);
+        (async() =>{
+            const expandedQuerryTokens = await this.querrySynonyms(querryTokens);
+            this.documentCollection.documents.forEach(document => {
+                const distances = [];
+                const wordMatches = [];
+                const documentTokens = this.stemTokens(document.content); 
+                const stemmedQuerryTokens = this.stemTokens(expandedQuerryTokens);
+                
+               
+                stemmedQuerryTokens.forEach(querryToken => {
+                const tokenDistances = documentTokens.map(documentToken => 
+                        this.calculateLevenshteinDistance(querryToken, documentToken)
+                    );
+                    const minDistance = Math.min(...tokenDistances);
+                    distances.push(minDistance)
+                    const wordMatch = documentTokens[tokenDistances.indexOf(minDistance)];
+                    wordMatches.push(wordMatch);
+                });
+                //distance between two strings according to insertion, deletion and substitution
+                const averageDistance = distances.reduce((sum, distance) => sum + distance, 0) / distances.length;
+                if (averageDistance<3){
+                    console.log(averageDistance)
+                    console.log(`Document: ${document.title}, querry found: ${querry}`);
+                    console.log(`closest matches to this word: ${wordMatches.join(', ')}`)
+                }     
             });
-            //distance between two strings according to insertion, deletion and substitution
-            const averageDistance = distances.reduce((sum, distance) => sum + distance, 0) / distances.length;
-            if (averageDistance<3){
-                console.log(averageDistance)
-                console.log(`Document: ${document.title}, querry found: ${querry}`);
-                console.log(`closest matches to this word: ${wordMatches.join(', ')}`)
-            }     
-        });
+        })();
 
     }
 }
@@ -99,13 +100,14 @@ const documentCollection = new DocumentCollection();
 
 documentCollection.addDocumentStoredLocally(filePaths);
 
+const searchEngin = new SearchEngine(documentCollection);
+
+
+
 const document1 = documentCollection.getDocumentByTitle('fastFood.txt');
 const document2 = documentCollection.getDocumentByTitle('bikes.txt')
 
-const searchEngin = new SearchEngine(documentCollection);
-
-searchEngin.search("burgers");
-
+searchEngin.search("bicycle");
 
 
 // console.log('document 1: ', document1);
